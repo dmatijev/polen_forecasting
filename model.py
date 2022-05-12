f0e2c5a3eaaa87fa41b265b0859248e7d9b2efbb
Domagoj Matijevic <domagoj@mathos.hr>
2022-05-12 14:53:28 +0200

    an ongoing work on new attention layer

--- model.py
+++ model.py
@@ -1,6 +1,61 @@
 import torch
 import torch.nn as nn
 
+class Attention1(nn.Module):
+    def __init__(self, hidden_dim):
+        super().__init__()
+        self.W = nn.Linear(hidden_dim, hidden_dim) # linear combination coefficients (i.e. attention weights)
+        self.hidden_dim = hidden_dim
+
+    def forward(self, hx, encoder_states): # dimension 
+        att_weights = torch.bmm(encoder_states, self.W(hx).unsqueeze(2)).squeeze(2)        
+        return att_weights
+        
+    
+
+
+class Net1(nn.Module):
+    def __init__(self, input_dim, hidden_dim, seq_len, nr_days = 1, n_layers=1, attention = False, device = torch.device("cpu")):
+        super(Net1, self).__init__()
+        self.lstm = nn.LSTM(input_size = input_dim, hidden_size = hidden_dim, num_layers = n_layers, bias=True, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, 1)
+        self.lstm_cell = nn.LSTMCell(6, hidden_dim) # 6 different meteo data
+        self.sigmoid = nn.Sigmoid()
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        self.use_attention_layer = attention
+        self.attention = Attention1(hidden_dim)
+        self.nr_days = nr_days
+        self.device = device
+        self.input_dim = input_dim
+        
+    def forward(self, x, meteo, shared_weights = False):# hidden):    
+
+ 
+        all_encoder_states, hidden = self.lstm(x)#, hidden)
+
+        encoder_out = hidden[0].contiguous().view(-1, self.hidden_dim) # take only the last hidden state
+            
+
+        if shared_weights:
+            encoder_out = torch.mean(encoder_out, 0, True)
+        
+        hx = encoder_out
+        cx = torch.zeros(encoder_out.shape).to(self.device)
+        out = []
+        for i in range(self.nr_days): # start decoding
+            hx, cx = self.lstm_cell(meteo[:,i,:], (hx, cx))
+            Hx = self.attention(hx, all_encoder_states) # TODO IMPLEMENT THIS
+            # ux = [hx, Hx]
+            # vx = W*ux
+            # ox = dropout(tanh(vx)) \in R^h
+            # out.append(self.fc(ox))
+            
+            out.append(self.fc(hx))
+
+        return torch.cat(out, dim = 1)
+ 
+
 
 class Attention(nn.Module):
     def __init__(self, input_dim):
@@ -12,6 +67,8 @@ class Attention(nn.Module):
         states_reshaped = torch.swapaxes(encoder_states, 1, 2)
         return self.v(states_reshaped)
         
+    
+
 
 class Net(nn.Module):
     def __init__(self, input_dim, hidden_dim, seq_len, nr_days = 1, n_layers=1, attention = False, device = torch.device("cpu")):
